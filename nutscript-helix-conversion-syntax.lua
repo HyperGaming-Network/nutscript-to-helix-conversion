435b80e2932d5a48dd7af48d3014fd57b2b145b0
Ireheart <sir.ireheart@gmail.com>
2020-12-28 07:04:24 +0000

    Create nutscript-helix-conversion-syntax.lua

---
+++ nutscript-helix-conversion-syntax.lua
@@ -0,0 +1,44 @@
+nut.
+ix.
+
+.util.include
+.util.Include
+
+.util.getMaterial
+.util.GetMaterial
+
+getChar()
+GetCharacter()
+
+getNetVar(
+GetNetVar(
+
+setNetVar(
+SetNetVar(
+
+.command.add
+.command.Add
+
+getInv()
+GetInv()
+
+getItems(
+GetItems(
+
+
+Item changes:
+
+onClick
+OnClick
+
+onCanRun
+OnCanRun
+
+onRun =
+OnRun =
+
+getData(
+GetData(
+
+ITEM:hook(
+ITEM:Hook(
